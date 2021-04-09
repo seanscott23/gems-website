b8cb42c1ff47483e51ae3848c34a0953806e0eac
Simonsays <simonsaysfollow@gmail.com>
2021-04-09 15:53:09 -0700

    begun working on model display

---
+++ src/components/pages/AudioModelRipper.tsx
@@ -0,0 +1,15 @@
+import "../../styles/AudioModelRipper.css"
+import React, { useEffect, useState } from 'react';
+
+function AudioModelRipper() {
+
+    return(
+        <div id="myModal" className="modal">
+            <div className="modal-content">
+                <span className="close">&times;</span>
+                <p>Some text in the Modal..</p>
+            </div>
+        </div>
+    );
+}
+export default AudioModelRipper;
