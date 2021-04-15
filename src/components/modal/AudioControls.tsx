f3a97925c71936afdaddd7f2b5b422b3632ce9c1
thewizard23 <seanpscott6@gmail.com>
2021-04-14 19:44:13 -0700

    Mostly just struggled finding how to update range

--- src/components/modal/AudioControls.tsx
+++ src/components/modal/AudioControls.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Slider, { Range } from "rc-slider";
 import "rc-slider/assets/index.css";
 import { useRef } from "react";
+import { debug } from "webpack";
+import { ProgressBar } from "react-bootstrap";
 
 export const Controls: React.FC<{
   audioMetaData: HTMLAudioElement | undefined;
@@ -11,7 +13,11 @@ export const Controls: React.FC<{
   const toggle: HTMLButtonElement | null = document.querySelector(".toggle");
   const [startTime, setStartTime] = React.useState<number>(0);
   const [endTime, setEndTime] = React.useState<number>(100);
-
+  const rightProgressBar:
+    | HTMLCollectionOf<Element>
+    | any = document.getElementsByClassName(
+    "rc-slider-handle rc-slider-handle-1 rc-slider-handle-dragging rc-slider-handle-click-focused"
+  );
   const { Range } = Slider;
   // let timeStamp = useRef(audioMetaData);
   // const {duration} = timeStamp.current
@@ -25,14 +31,12 @@ export const Controls: React.FC<{
     if (audioMetaData?.duration != undefined) {
       setEndTime(calculateMinFromSec(audioMetaData?.duration));
     }
-    const timer: ReturnType<typeof setTimeout>;
-    if(isPlaying){
-     timer =
-      setInterval(() => 
-        setStartTime(startTime + 1)
-      , 1000);
-    }else{return () => clearInterval(timer)}
-   
+    let timer: ReturnType<typeof setTimeout>;
+    if (isPlaying) {
+      timer = setInterval(() => setStartTime(startTime + 1), 1000);
+    } else {
+      return () => clearInterval(timer);
+    }
   }, [audioMetaData, startTime]);
 
   const togglePlay = () => {
@@ -44,12 +48,44 @@ export const Controls: React.FC<{
     }
   };
 
+  // const playNew = (source: string, time: number) => {
+  //   let newAudio = new Audio(source);
+  //   newAudio.loop = true;
+  //   newAudio.play();
+  //   setTimeout(() => {
+  //     newAudio.pause();
+  //   }, time);
+  // };
+
   const onMinChange = (e: number[]) => {
     setStartTime(e[0]);
     setEndTime(e[1]);
+    // if (rightProgressBar[0]) {
+    //   rightProgressBar[0].ariaValueNow = e[1];
+    // }
+
+    // parseFloat(rightProgressBar[0].getAttribute("aria-valuenow"))
+  };
+  // let currentTime: number = 0;
+  // if (audio) {
+  //   currentTime = audio.currentTime;
+  // }
+  const updateScrubTime = (e: number[]) => {
+    console.log(e[0]);
+
+    if (audio) {
+      audio.oncanplay = () => {
+        // result = audio.src.split("#t=");
+        // final = result[0] + "#t=" + startTime;
+        // let newAudio = new Audio(final);
+        // newAudio.loop = true;
+        // newAudio.play();
+        audio.currentTime = startTime;
+      };
+    }
   };
 
-
+  // debugger;
   return audioMetaData ? (
     <div className="player__controls">
       <div id="rc-sliderDiv">
@@ -61,6 +97,7 @@ export const Controls: React.FC<{
           min={0}
           max={parseFloat((audioMetaData.duration / 60).toFixed(2))}
           onChange={(e) => onMinChange(e)}
+          onAfterChange={(e) => updateScrubTime(e)}
         />
       </div>
       <div className="control-bar">
