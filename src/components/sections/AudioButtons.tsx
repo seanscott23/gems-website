d37d30ed4bd64429d1f6106436a26534bb86d623
thewizard23 <seanpscott6@gmail.com>
2021-04-23 14:24:17 -0700

    Added in imports for buttons

--- src/components/sections/AudioButtons.tsx
+++ src/components/sections/AudioButtons.tsx
@@ -1,5 +1,9 @@
 import React, { FC, useState, useRef } from "react";
 import { Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { submitNewClip } from "../../store/actions/authActions";
+import AudioModalRipper from "../modal/AudioModalRipper";
 
 interface Clip {
   title: string;
@@ -11,31 +15,82 @@ export const AudioButtons: React.FC<{
   audioMetaData: HTMLAudioElement | undefined;
 }> = ({ audioMetaData }) => {
   const [show, setShow] = React.useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
   const [isModalOpen, setModalState] = React.useState(false);
+  const [begin, setBegin] = React.useState(0);
+  const [end, setEnd] = React.useState(0);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  let clip;
+  if (audioMetaData) {
+    clip = { title: "", enclosure: { url: audioMetaData.src } };
+  }
 
   React.useEffect(() => {
     setShow(true);
     // if(audioMetaData.duration)
   }, [audioMetaData]);
 
-  return audioMetaData ? (
-    <Button
-      className="crop-upload"
-      onClick={() => {
-        setModalState(!isModalOpen);
-      }}
-    >
-      Crop audio
-    </Button>
-  ) : (
-    <Button
-      className="upload-audio"
-      onClick={() => {
-        setModalState(!isModalOpen);
-      }}
-    >
+  const handleTimeUpdate = (startPoint: number, endPoint: number) => {
+    setBegin(startPoint);
+    setEnd(endPoint);
+  };
+  const handleClose = () => {
+    setModalState(!isModalOpen);
+  };
+
+  const submitHandler = async (
+    e: React.MouseEvent<HTMLElement, MouseEvent>
+  ) => {
+    if (audioMetaData) {
+      e.preventDefault();
+      setLoading(true);
+      await dispatch(
+        submitNewClip(
+          audioMetaData?.src,
+          audioMetaData.currentTime,
+          audioMetaData?.duration
+        )
+      );
+      setLoading(false);
+      history.push("/gem-form");
+    }
+  };
+
+  const getAudioTime = () => {
+    if (audioMetaData) {
+      return audioMetaData.duration <= 600 ? true : false;
+    } else {
+      return false;
+    }
+  };
+  debugger;
+  return getAudioTime() ? (
+    <Button className="upload-audio" onClick={submitHandler}>
       Upload audio
     </Button>
+  ) : (
+    <div>
+      <Button
+        className="crop-audio"
+        onClick={() => {
+          setModalState(!isModalOpen);
+        }}
+      >
+        Crop audio
+      </Button>
+      {clip ? (
+        <AudioModalRipper
+          isOpen={isModalOpen}
+          handleClose={handleClose}
+          clip={clip}
+          id={1}
+          begin={begin}
+          end={end}
+          handleTimeUpdate={handleTimeUpdate}
+        />
+      ) : null}
+    </div>
   );
 };
 
