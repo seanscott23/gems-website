619be23ebd0e29de09d7d6f781c2ac6f03a15ace
thewizard23 <seanpscott6@gmail.com>
2021-04-23 19:33:15 -0700

    Updated container and gem form styles

--- src/components/pages/GemUploadForm.tsx
+++ src/components/pages/GemUploadForm.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-
+import "../../styles/GemForm.css";
+import Button from "../UI/Button";
 const GemForm = () => {
   const { gemURL } = useSelector((state: RootState) => state.auth);
+
+  const submitFinalGem = () => {};
+
   return (
     <div className="gem-container">
       <h1>Upload Gem Form</h1>
@@ -13,24 +17,33 @@ const GemForm = () => {
       <Form.Group>
         <Form.Control type="text" placeholder="Title" />
         <br />
+        <Form.Control
+          as="textarea"
+          rows={3}
+          placeholder="Description"
+          className="gem-description"
+        />
 
-        <Form.Control as="textarea" rows={3} placeholder="Description" />
-        <br />
-        <Form.Group controlId="exampleForm.ControlSelect2">
-          <Form.Label>Select Categories</Form.Label>
-          <Form.Control as="select" multiple>
-            <option>Comedy</option>
-            <option>News</option>
-            <option>History</option>
-            <option>Science</option>
-            <option>Music</option>
-          </Form.Control>
-        </Form.Group>
+        <Form.Label>
+          Select Categories (commmand + click to select multiple)
+        </Form.Label>
+        <Form.Control as="select" multiple id="gem-categories">
+          <option>Comedy</option>
+          <option>News</option>
+          <option>History</option>
+          <option>Science</option>
+          <option>Music</option>
+        </Form.Control>
         <br />
         <Form.Check
           type="checkbox"
           label="Check box if explicit"
           id={`disabled-default-checkbox`}
+        />
+        <Button
+          className="gem-form-button"
+          onClick={submitFinalGem}
+          text="Submit Gem"
         />
       </Form.Group>
     </div>
