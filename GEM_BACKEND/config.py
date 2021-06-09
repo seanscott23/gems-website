b658a24e94c9f09ef2e0551ea7c06fa4a32760c4
thewizard23 <seanpscott6@gmail.com>
2021-06-09 15:56:01 -0700

    Updated mobile styles with media queries

--- GEM_BACKEND/config.py
+++ GEM_BACKEND/config.py
@@ -6,14 +6,14 @@ load_dotenv()
 
 class Settings(BaseSettings):
     
-    API_KEY_TEST: str = os.getenv.get('API_KEY_TEST')
-    AUTH_DOMAIN_TEST: str = os.getenv.get('AUTH_DOMAIN_TEST')
-    DATABASE_URL_TEST: str = os.getenv.get('DATABASE_URL_TEST')
-    PROJECT_ID_TEST: str = os.getenv.get('PROJECT_ID_TEST')
-    STORAGE_BUCKET_TEST: str = os.getenv.get('STORAGE_BUCKET_TEST')
-    MESSAGING_SENDER_ID_TEST: str = os.getenv.get('MESSAGING_SENDER_ID_TEST')
-    APP_ID_TEST: str = os.getenv.get('APP_ID_TEST')
-    MEASUREMENT_ID_TEST: str = os.getenv.get('MEASUREMENT_ID_TEST')
+    API_KEY_TEST: str = os.getenv('API_KEY_TEST')
+    AUTH_DOMAIN_TEST: str = os.getenv('AUTH_DOMAIN_TEST')
+    DATABASE_URL_TEST: str = os.getenv('DATABASE_URL_TEST')
+    PROJECT_ID_TEST: str = os.getenv('PROJECT_ID_TEST')
+    STORAGE_BUCKET_TEST: str = os.getenv('STORAGE_BUCKET_TEST')
+    MESSAGING_SENDER_ID_TEST: str = os.getenv('MESSAGING_SENDER_ID_TEST')
+    APP_ID_TEST: str = os.getenv('APP_ID_TEST')
+    MEASUREMENT_ID_TEST: str = os.getenv('MEASUREMENT_ID_TEST')
 
     class Config:
         env_file = ".env"
